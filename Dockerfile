8f36e27bdcd48e816f8fce5a0a46ea3b19deb03d
Anton Mitsengendler <anton.mitsengendler@outlook.com>
2026-03-03 17:21:24 +0100

    entrypoint script collects natinfo and passes it to the proxy

--- Dockerfile
+++ Dockerfile
@@ -18,32 +18,28 @@ FROM ubuntu:latest
 ENV CONFIG_PATH="/data"
 # install runtime deps
 RUN apt -y update; \
-    apt -y install cron xxd curl ntp; \
+    apt -y install cron xxd curl; \
     apt -y clean
 
 # copy artifacts from build stage
 COPY --from=build /mtproto/objs/bin/ /mtproxy
 COPY start.sh /mtproxy
-RUN mkdir /data
+RUN mkdir ${CONFIG_PATH}
 
 WORKDIR /mtproxy
 RUN chmod +x start.sh
-
-# download key and config
-RUN curl -s https://core.telegram.org/getProxySecret -o ${CONFIG_PATH}/proxy-secret; \
-    curl -s https://core.telegram.org/getProxyConfig -o ${CONFIG_PATH}/proxy-multi.conf
 
 # setup cron to periodically fetch tg configuration as it may change
 RUN (crontab -l 2>/dev/null; echo "0 3 * * * curl -s https://core.telegram.org/getProxySecret -o ${CONFIG_PATH}/proxy-secret >> /var/log/cron.log 2>&1") | crontab - ;\
     (crontab -l 2>/dev/null; echo "0 3 * * * curl -s https://core.telegram.org/getProxyConfig -o ${CONFIG_PATH}/proxy-multi.conf >> /var/log/cron.log 2>&1") | crontab - ;\
 # fetch stats from a local endpoint
-    (crontab -l 2>/dev/null; echo "0 3 * * * curl -s localhost:8888/stats -o /mtproxy/stats/$(date +%d.%m.%y).log >> /var/log/cron.log 2>&1") | crontab - ;\
+    (crontab -l 2>/dev/null; echo "0 3 * * * curl -s localhost:8888/stats -o ${CONFIG_PATH}/stats/$(date +%d.%m.%y).log >> /var/log/cron.log 2>&1") | crontab - ;\
 # restart proxy to allow update configuration if necessary
     (crontab -l 2>/dev/null; echo "0 4 * * *  pkill -f mtproto-proxy  >> /var/log/cron.log 2>&1") | crontab - ;
 
 # ports
 EXPOSE 8888 443/tcp 443/udp
 # healthcheck
-HEALTHCHECK --interval=60s --timeout=30s --start-period=10s CMD curl -f http://localhost:8888/stats || exit 1
+HEALTHCHECK --interval=180s --timeout=30s --start-period=10s CMD curl -f http://localhost:8888/stats || exit 1
 # starting mtproto-proxy
 ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,37 +1,55 @@
 #!/bin/sh
 
+# set variables
+[ -z "$CONFIG_PATH" ] && CONFIG_PATH="/data"
+[ -z "$CLIENT_PORT" ] && CLIENT_PORT=443
+[ -z "$EXT_PORT"] && EXT_PORT=14443
 USER_SECRETS_FILE="${CONFIG_PATH}/user-secrets.txt"
+EXT_IP=$(curl ifconfig.co/ip -s)
+INT_IP=$(hostname -I | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
+USER_SECRETS_COUNT=5
 
-# Start NTP service
-service ntpsec start
+# Download TG key and proxy configuration if they don't exist
+if [ ! -f "${CONFIG_PATH}/proxy-secret" ]; then
+curl -s https://core.telegram.org/getProxySecret -o ${CONFIG_PATH}/proxy-secret
+fi
+if [ ! -f " ${CONFIG_PATH}/proxy-multi.conf" ]; then
+curl -s https://core.telegram.org/getProxyConfig -o ${CONFIG_PATH}/proxy-multi.conf
+fi
+
 
 # Check if user secrets need to be generated
 if [ ! -f "$USER_SECRETS_FILE" ]; then
     echo "User secrets file not found. Creating new file and generating secrets..."
-    for i in $(seq 1 10); do
+    for i in $(seq 1 $USER_SECRETS_COUNT); do
         head -c 16 /dev/urandom | xxd -ps >> "$USER_SECRETS_FILE"
     done
     echo "Done."
-    echo "
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-NEW SECRETS GENERATED. USE THEM WISELY
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-"
-    cat $USER_SECRETS_FILE
-    echo "
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-"
 fi
 
 # Parse the user secrets file
+echo "
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+USE THESE LINKS WISELY
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+
 USER_SECRETS=""
 while IFS= read -r key; do
     # skip empty lines
     [ -z "$key" ] && continue 
     USER_SECRETS="$USER_SECRETS -S $key"
+    echo "https://t.me/proxy?server=$EXT_IP&port=$EXT_PORT&secret=$key"
 done < "$USER_SECRETS_FILE"
+echo "
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
 
+# Run cron for automatic config updates
+cron
+
+# Run proxy
 exec ./mtproto-proxy -u nobody -p 8888 -H 443 \
      --aes-pwd ${CONFIG_PATH}/proxy-secret ${CONFIG_PATH}/proxy-multi.conf \
      --http-stats \
-     $USER_SECRETS
+     --nat-info ${INT_IP}:${EXT_IP}
+     -M 1
+     ${USER_SECRETS}
